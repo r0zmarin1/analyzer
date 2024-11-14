d69e036a5dd7cd7cfb85dbb051aeaddff483362d
AA <Student@CAB4-2>
2024-11-14 15:03:34 +1000

    Добавьте файлы проекта.

---
+++ analyzer/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace analyzer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<ServiceResults> ServicesResults { get; set; }
+        public ServiceResults ServiceResults { get; set; }
+        private DispatcherTimer timer = null;
+        HttpClient httpClient = new HttpClient();
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            httpClient.BaseAddress = new Uri("http://localhost:5000/api/");
+            timerStart();
+        }
+
+        public void timerStart()
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = new TimeSpan(0, 0, 5);
+            timer.Start();
+        }
+
+        private void timerTick(object sender, EventArgs e) //к таймеру относится 
+        {
+            Thread thread = new Thread(GetResultsFromLedetect);
+            thread.Start();
+        }
+        private void NewAnalys(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public async void GetResultsFromLedetect()
+        {
+            var responce = await httpClient.GetAsync($"analyzer/Ledetect");
+            if (responce.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                var result = await responce.Content.ReadAsStringAsync();
+                MessageBox.Show(result);
+                return;
+            }
+            else
+            {
+                var result = await responce.Content.ReadFromJsonAsync<ServiceResults>();
+                ServiceResults = new ServiceResults
+                {
+                    Code = result.Code,
+                    PatientId = result.PatientId,
+                    Result = result.Result,
+                    Progress = result.Progress
+                };
+                ServicesResults.Add(ServiceResults);
+            }
+        }
+    }
+}
---
+++ analyzer/ServicesResults.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace analyzer
+{
+    public class ServiceResults
+    {
+        //public int Id { get; set; }
+        public int PatientId { get; set; } = 0;
+        public int Code { get; set; } = 0;
+        public string Result { get; set; } = string.Empty;
+        public int Progress { get; set; } = 0; 
+    }
+}
